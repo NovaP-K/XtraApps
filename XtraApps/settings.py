5926d7a9a9ca45dad50aaecae1cc98262c99de03
NovaPrince <46784613+DarkGomasu@users.noreply.github.com>
2021-04-12 22:00:00 +0530

    Update settings.py

--- XtraApps/settings.py
+++ XtraApps/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'XtraApps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = { 'default': dj_database_url.config() }
 
 #DATABASES = {
 #   'default': {
